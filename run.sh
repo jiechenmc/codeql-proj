7e413e1d15475793f78e01e923526ec33497c58d
Jie Chen <jiechenmc@gmail.com>
2025-06-04 23:01:05 -0400

    save work

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
+: "${1:?First argument is required; Supply Query Name}"
+
 query_dir="./codeql-custom-queries-python"
 
-
+if [[ ! -f "$query_dir/$1.ql" ]]; then
+  echo "Error: File '$query_dir/$1.ql' not found." >&2
+  exit 1
+fi
 
 codeql database create db --language python --source-root lambda --overwrite
-codeql query run codeql-custom-queries-python/$1.ql --database ./db
+codeql query run $query_dir/$1.ql --database ./db

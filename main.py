460dc919ee0358c90ac2bfbce34c6914cd33497f
Jie Chen <jiechenmc@gmail.com>
2025-06-15 13:18:44 -0500

    compile verbs

--- main.py
+++ main.py
@@ -21,3 +21,73 @@ with open(filename, "r") as f:
     tree = ast.parse(f.read())
 
 HelloArgVisitor().visit(tree)
+
+
+services = [
+    "s3",
+    "dynamodb",
+    "lambda",
+    "ssm",
+    "sqs",
+    "sns",
+    "ec2",
+    "sts",
+    "rekognition",
+    "cloudformation",
+    "stepfunctions",
+]
+
+sink_common_verbs = [
+    "batch",
+    "put",
+    "create",
+    "delete",
+    "download",
+    "copy",
+    "upload",
+    "cancel",
+    "update",
+    "execute",
+    "query",
+    "scan",
+    "invoke",
+    "add",
+    "remove",
+    "terminate",
+    "send",
+    "change",
+    "purge",
+    "set",
+    "confirm",
+    "subscribe",
+    "publish",
+    "opt",
+    "check",
+    "list",
+    "run",
+    "associate",
+    "accept",
+    "allocate",
+    "assign",
+    "detatch",
+    "disable",
+    "enable",
+    "release",
+    "purchase",
+    "modify",
+    "reset",
+    "revoke",
+    "restore",
+    "unassign",
+    "search",
+    "assume",  # sts
+    "decode",  # sts
+    "detect",  # sts
+    "recognize",  # sts
+    "activate",  # cloudformation
+    "import",
+    "describe",
+    "signal",
+]
+
+source_common_verb = ["receive", "get"]
--- methods.py
+++ methods.py
@@ -1,5 +1,36 @@
 import boto3
+import bs4
+import os
+from pathlib import Path
 
-client = boto3.client("s3")
-operations = client.meta.service_model.operation_names
-print(operations)
+# client = boto3.client("s3")
+# operations = client.meta.service_model.operation_names
+# print(operations)
+
+serverless_repo_location = "Serverless_Repositories"
+
+
+def find_serverless_yaml(search_path: str, filename: str = "serverless.yml"):
+    for root, dirs, files in os.walk(search_path):
+        if filename in files:
+            return os.path.join(root, filename)
+    return None
+
+
+def analyze(folder_name: str):
+    search_path = f"{serverless_repo_location}/{folder_name}"
+    # print(search_path)
+    # print(find_serverless_yaml(search_path))
+
+    for p in Path(search_path).rglob("*.py"):  # Use "*.txt" for filtering
+        print(p)
+
+
+def main():
+    folders = os.listdir(serverless_repo_location)
+    for folder in folders:
+        analyze(folder)
+
+
+if __name__ == "__main__":
+    main()
